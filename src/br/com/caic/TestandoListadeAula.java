fd90bd0ef385b3cc87511d0426557334bbc94dd6
Caic Souza <Caic Souza@192.168.25.153>
2017-11-02 13:52:19 -0200

    Utilizando compareTo, toString.

---
+++ src/br/com/caic/TestandoListadeAula.java
@@ -0,0 +1,36 @@
+package br.com.caic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Comparison;
+
+public class TestandoListadeAula {
+	
+	public static void main(String[] args) {
+		
+		Aula a1 = new Aula("japones - software", 50);
+		Aula a2 = new Aula("Aula Taynara Gerenciamento", 20);
+		Aula a3 = new Aula("matematica aplicado", 70);
+		
+		ArrayList<Aula> aulas = new ArrayList<>();
+		aulas.add(a1);
+		aulas.add(a2);
+		aulas.add(a3);
+		
+		System.out.println(aulas);
+		
+		System.out.println("Lista Ordenada");
+		//funciona porque foi adicionado uma metodo compareTo na classe aula;
+		Collections.sort(aulas);
+		System.out.println("COMPARAR PELO TITULO");
+		System.out.println(aulas);
+		
+		//Fazer comparação pelo tempo de video
+		Collections.sort(aulas, Comparator.comparing(Aula::getTempo));
+		
+		System.out.println("COMPARAR PELO TEMPO");
+		System.out.println(aulas);
+	}
+}
